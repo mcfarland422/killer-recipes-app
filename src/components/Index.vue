01748a107ed202bd5cef0d6cea2dfece67ebd86b
Michael McFarland <michael.mcfarland@luckie.com>
2019-05-06 15:04:47 -0400

    first

---
+++ src/components/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      recipes: [
+        { title: 'Ninja Brew Smoothie', slug: 'ninja-brew', ingredients: ['bananas', 'coffee', 'almond milk'], id},
+        { title: 'Morning Mood Smoothie', slug: 'morning-mood', ingredients: ['mango', 'lime', 'juice'], id},
+        { title: 'Ninja Brew Smoothie', slug: 'ninja-brew', ingredients: ['bananas', 'coffee', 'almond milk'], id},
+        
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
